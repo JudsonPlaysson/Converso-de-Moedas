02109a238e2d4469fad325f1394111bd941053b3
JudsonPlaysson <judson_playsson@hotmail.com>
2025-03-20 22:07:29 -0300

    Acrescentou mais moedas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const convertButton = document.querySelector(".convert-button")
 const selectCurrency = document.querySelector(".select-currency")
+const currencyValor = document.querySelector(".selectValor")
 
 function cliquei() {
     const input = document.querySelector(".input-currency").value
@@ -8,6 +9,8 @@ function cliquei() {
     const valorReal = document.querySelector(".para2")
     const valorDolar = 5.2
     const valorEuro = 6.2
+    const valorLibra = 7.32
+    const valorBitcoin = 85.773
 
     valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -26,8 +29,27 @@ function cliquei() {
             currency: "EUR"
         }).format(input / valorEuro)
     }
+    if (selectCurrency.value == 'libra') {
+        conversorDolar.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(input / valorLibra)
+    }
+    if (selectCurrency.value == "bitcoin") {
+        conversorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: 'BTC'
+        }).format(input / valorBitcoin)
+    }
+    if (selectCurrency.value == 'brazil') {
+        conversorDolar.innerHTML = new Intl.NumberFormat('pt-BR', {
+            style: "currency",
+            currency: "BRL"
+        }).format(input)
+    }
 
 }
+
 
 function changeCurrency() {
     const currencyName = document.querySelector('.para3')
@@ -43,9 +65,24 @@ function changeCurrency() {
         currencyImg.src = "./assets/euro.png"
     }
 
+    if (selectCurrency.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImg.src = "./assets/libra 1.png"
+    }
+
+    if (selectCurrency.value == "bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin 1.png"
+    }
+
+    if (selectCurrency.value == "brazil") {
+        currencyName.innerHTML = "Real"
+        currencyImg.src = "./assets/brasil 2.png"
+    }
+
+
     cliquei()
 }
-
 
 selectCurrency.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", cliquei)
